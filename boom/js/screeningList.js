107bba6f63b956d60254bd0afe66f6f63d0a751d
zmx2321@163.com <zmx2321@163.com>
2016-11-02 22:16:47 +0800

    gx

--- boom/js/screeningList.js
+++ boom/js/screeningList.js
@@ -6,4 +6,15 @@ $(function(){
 	$(".list_seasrch b").click(function(){
 		$(".list_seasrch").fadeOut();
 	});
+
+	//选项卡
+	$(function(){
+		$(".scrlit_nav li").click(function(){
+			//alert(1);
+			$(this).addClass("scrlit_nav_on").siblings().removeClass("scrlit_nav_on");
+			$('.scrlit_article section').hide();
+			id=$(this).attr('id');
+			$("."+id+"_box").show();
+		});
+	});
 });
